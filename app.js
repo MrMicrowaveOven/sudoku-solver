10aebd4446cfdba6ff39cf618b76743a319010de
Benjamin Zagorski <benj@zagorski.com>
2020-01-12 10:46:40 -0800

    Grid is now constructed in a respectful manner

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 // Build Sudoku Grid
-    // for (var i = 0; i < array.length; i++) {
-    //   array[i]
-    // }
+var text_field = '<input type="text" name="" value="" class="sudoku-space">'
+for (var i = 0; i < 9; i++) {
+  var div = '<div class="row' + i + '"></div>'
+  $('.sudoku-grid').append(div)
+  for (var j = 0; j < 9; j++) {
+    $('.row' + i).append(text_field)
+  }
+  $('.row' + i).append('<br>')
+}
 
 
 $.ajax({
