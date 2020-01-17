29b28ebe9c975650edab1dd978e8e094e26d286e
Benjamin Zagorski <benj@zagorski.com>
2020-01-17 13:16:09 -0800

    Added basic styling to sudoku grid

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 // Build Sudoku Grid
-var text_field = '<input type="text" name="" value="" class="sudoku-space">'
 for (var i = 0; i < 9; i++) {
   var div = '<div class="row' + i + '"></div>'
   $('.sudoku-grid').append(div)
   for (var j = 0; j < 9; j++) {
+    var text_field = '<input type="text" id="space' + i + j + '" name="" value="" class="sudoku-space" oninput="this.value = this.value.replace(/[^0-9.]/g, ``).replace(/(\..*)\./g, `$1`);">'
     $('.row' + i).append(text_field)
   }
   $('.row' + i).append('<br>')
 }
+
+$('.sudoku-space').each(function(i, el) {
+  var classList = $(this).attr('id').split('space')
+  var spaceId = classList[classList.length - 1]
+  var firstDigit = spaceId[0]
+  var secondDigit = spaceId[1]
+  var isGray = firstDigit < 3 && secondDigit < 3
+  isGray += firstDigit < 3 && secondDigit > 5
+  isGray += (firstDigit >= 3 && firstDigit <= 5) && (secondDigit >= 3 && secondDigit <= 5)
+  isGray += firstDigit > 5 && secondDigit < 3
+  isGray += firstDigit > 5 && secondDigit > 5
+  if (isGray) {
+    $(this).addClass('gray')
+  }
+
+})
+
 
 
 $.ajax({
