ef411a5bdaa8648485b230e1f415ab0e9f766263
Benjamin Zagorski <benj@zagorski.com>
2020-01-19 12:23:06 -0800

    Now using render_template

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ from script import parse_board
 @app.route('/', methods=["POST", "GET"])
 def main_interface():
     if request.method == 'GET':
-        content = get_file('index.html')
-        return Response(content, mimetype="text/html")
+        return render_template("index.html", title = 'Index')
     elif request.method=='POST':
         response = request.get_json()
         board = response['board']
